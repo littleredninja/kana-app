986905213021ee01fe0a1351ab5550aa9eb49809
Jennie Chamberlin <jennie@trunkclub.com>
2018-06-01 23:20:08 -0500

    adding kana display, input and check

--- src/App.js
+++ src/App.js
@@ -1,16 +1,68 @@
 import React, { Component } from 'react'
+import { DICTIONARY } from './constants'
 import './App.css'
 
 class App extends Component {
+  state = {
+    library: 'hiragana',
+    question: '',
+    answerKey: '',
+    isAnswer: null,
+  }
+
+  componentDidMount() {
+    this.getRandomKana()
+  }
+
+  getRandomKana = libraryId => {
+    const library = DICTIONARY[this.state.library]
+    const kana = library[Math.floor(Math.random() * library.length)]
+    this.setState({
+      question: kana[1],
+      answerKey: kana[0],
+      isAnswer: null,
+    })
+  }
+
+  selectLibrary = libraryId => {
+    this.setState({
+      library: libraryId,
+    })
+  }
+
+  onSubmit = evt => {
+    evt.preventDefault()
+    const answerInput = document.getElementById('answerInput').value
+    if (answerInput === this.state.answerKey) {
+      this.setState({
+        isAnswer: true,
+      })
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <header>Kana Practice</header>
+        <div class="main">
+          <div>{this.state.question}</div>
+          {this.state.isAnswer ? (
+            <div>
+              <div>That's right!</div>
+              <button onClick={() => this.getRandomKana()}>Next</button>
+            </div>
+          ) : (
+            <div>
+              <form>
+                {!this.state.isAnswer && <div>Nope!</div>}
+                <input id="answerInput" name="answerInput" type="text" />
+                <button onClick={this.onSubmit} type="submit">
+                  Check your answer
+                </button>
+              </form>
+            </div>
+          )}
+        </div>
       </div>
     )
   }
